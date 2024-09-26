f971d4804aeef29c12d1c185b5beb82389039ad5
richardR144 <richard.rigollet@lapiscine.pro>
2024-09-26 17:04:59 +0200

    problème de lien résolu

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,17 +4,22 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 class BaseController extends AbstractController
 {
-#[Route('/base', name:'base')]
-    public function base (Request $request): Response
+    #[Route('/base', name:'base')]
+    public function index (): Response
     {
-        return $this->render('base.html.twig');
+        return $this->render('Page1Accueil.html.twig');
+    }
+
+    #[Route('/page1Accueil', name: 'page1Accueil')]
+    public function page1Accueil(): Response
+    {
+        return $this->render('Page1Accueil.html.twig');
     }
 }
 
